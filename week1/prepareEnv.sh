d575c545c79863ab04c1cbd721c8f272a86e5c43
php3ik <oles.petriv@gmail.com>
2017-09-08 19:08:16 +0300

    first week: extracting visual features using pretrained models and creating contextual image search based on image query

---
+++ week1/prepareEnv.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get -y install python-pip
+apt-get -y install ipython ipython-notebook
+pip install numpy
+pip install opencv-python
+pip install -U scikit-learn
+pip install --upgrade tensorflow-gpu
+pip install keras
+pip install pandas
+pip install scipy
