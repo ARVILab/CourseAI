43607e53e128aaa6da56a60998355ce7997a07d3
php3ik <oles.petriv@gmail.com>
2017-09-26 17:12:10 +0300

    segnet, Unet, vggUnet

---
+++ week2/segnet.py
@@ -0,0 +1,83 @@
+from keras.layers.core import Layer, Activation,  Reshape, Permute
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D
+from keras import models
+
+
+def segnet(nClasses, optimizer=None, input_height=256, input_width=256):
+    kernel = 3
+    filter_size = 64
+    pool_size = 2
+
+    model = models.Sequential()
+    model.add(Layer(input_shape=(input_height, input_width, 3)))
+
+    # encoder
+    model.add(Convolution2D(filter_size, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
+
+    model.add(Convolution2D(128, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
+
+    model.add(Convolution2D(256, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
+
+    model.add(Convolution2D(512, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(512, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    # decoder
+    model.add(Convolution2D(512, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(512, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(UpSampling2D(size=(pool_size, pool_size)))
+    model.add(Convolution2D(256, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(UpSampling2D(size=(pool_size, pool_size)))
+    model.add(Convolution2D(128, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(UpSampling2D(size=(pool_size, pool_size)))
+    model.add(Convolution2D(filter_size, kernel, kernel, border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(nClasses, 1, 1, border_mode='same'))
+
+    model.outputHeight = model.output_shape[-3]
+    model.outputWidth = model.output_shape[-2]
+
+    model.add(Reshape((nClasses, model.outputHeight  * model.outputWidth),
+                      input_shape=(nClasses, model.outputHeight , model.outputWidth)))
+
+    model.add(Permute((2, 1)))
+    model.add(Activation('softmax'))
+
+    if not optimizer is None:
+        model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=['accuracy'])
+
+    return model
+
+if __name__ == '__main__':
+    m = segnet(81, optimizer='adam')
+    from keras.utils import plot_model
+    plot_model(m, show_shapes=True, to_file='modelGraphs/segnet.png')
+
---
+++ week2/vggUnet.py
@@ -0,0 +1,89 @@
+from keras.models import *
+from keras.layers import *
+from keras.utils.data_utils import get_file
+
+WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5'
+
+def VGGUnet(n_classes, input_height=416, input_width=608, vgg_level=3):
+    img_input = Input(shape=(input_height, input_width, 3))
+
+    # Block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    f1 = x
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    f2 = x
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    f3 = x
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    f4 = x
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+    f5 = x
+    vgg = Model(img_input, x)
+    weights_path = get_file('vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5',WEIGHTS_PATH_NO_TOP,cache_subdir='models')
+    vgg.load_weights(weights_path, by_name=True)
+
+    levels = [f1, f2, f3, f4, f5]
+
+    d = levels[vgg_level]
+
+    d = Conv2D(512, (3, 3), padding='same')(d)
+    d = BatchNormalization()(d)
+    d = Activation('relu')(d)
+
+    d = UpSampling2D((2, 2))(d)
+    d = Concatenate(axis=-1)([levels[vgg_level-1], d])
+    d = Conv2D(256, (3, 3), padding='same')(d)
+    d = BatchNormalization()(d)
+    d = Activation('relu')(d)
+
+    d = UpSampling2D((2, 2))(d)
+    d = Concatenate(axis=-1)([levels[vgg_level - 2], d])
+    d = Conv2D(128, (3, 3), padding='same')(d)
+    d = BatchNormalization()(d)
+    d = Activation('relu')(d)
+
+    d = UpSampling2D((2, 2))(d)
+    d = Concatenate(axis=-1)([levels[vgg_level - 3], d])
+    d = Conv2D(64, (3, 3), padding='same')(d)
+    d = BatchNormalization()(d)
+    d = Activation('relu')(d)
+
+    d = Conv2D(n_classes, (3, 3), padding='same')(d)
+    o_shape = Model(img_input, d).output_shape
+    outputHeight = o_shape[1]
+    outputWidth = o_shape[2]
+
+    d = (Reshape((-1, outputHeight * outputWidth)))(d)
+    d = (Permute((2, 1)))(d)
+    d = (Activation('softmax'))(d)
+    model = Model(img_input, d)
+    model.outputWidth = outputWidth
+    model.outputHeight = outputHeight
+
+    return model
+
+
+if __name__ == '__main__':
+    m = VGGUnet(81)
+    from keras.utils import plot_model
+    plot_model(m, show_shapes=True, to_file='modelGraphs/vggUnet.png')
