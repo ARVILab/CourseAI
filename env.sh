116f6d710f05922fed4e3b4b2913ca4f7c302e07
exipilis <malenko.andrii@gmail.com>
2017-09-26 16:29:40 +0300

    env

--- env.sh
+++ env.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+
 apt-get update
 apt-get -y install python-pip
 apt-get -y install ipython ipython-notebook
 apt-get -y install cython
+
 pip2 install cython
 pip2 install numpy
 pip2 install opencv-python
@@ -12,3 +14,4 @@ pip2 install keras
 pip2 install pandas
 pip2 install scipy
 pip2 install tsne
+pip2 install jupyter
