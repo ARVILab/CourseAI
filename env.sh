59738219251cb2a398894733f936ec3d14b351d2
exipilis <malenko.andrii@gmail.com>
2017-09-27 21:54:18 +0300

    env

--- env.sh
+++ env.sh
@@ -12,6 +12,7 @@ apt-get -y install libopenblas-dev libblas-dev libatlas-base-dev
 
 pip2 install --upgrade pip
 pip2 install cython
+pip2 install --upgrade jupyter
 pip2 install numpy
 pip2 install opencv-python
 pip2 install -U scikit-learn
@@ -19,12 +20,7 @@ pip2 install --upgrade tensorflow-gpu
 pip2 install keras
 pip2 install pandas
 pip2 install scipy
-pip2 install tsne
-pip2 install --upgrade jupyter
-pip2 install h5py
-pip2 install pillow
-pip2 install pylab
-pip2 install tsne
+pip2 install h5py, pillow, pylab, tsne, annoy
 
 python2 -m pip install ipykernel
 python2 -m ipykernel install --user
