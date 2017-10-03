580b5d8fafb1c101edaa5810696df646ab8e082f
exipilis <malenko.andrii@gmail.com>
2017-10-03 23:48:10 +0300

    correction

--- week1/6_finetune.py
+++ week1/6_finetune.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
 from keras.applications.resnet50 import ResNet50, preprocess_input
 from keras.models import Model
 from keras.layers import Dense, Flatten
 from keras.optimizers import SGD
 from keras.callbacks import ModelCheckpoint
-
-# кількість класів, підставте ваше значення
-nb_classes = 42
+from keras.preprocessing.image import ImageDataGenerator
 
 
 # модель = ResNet50 без голови з одним dense шаром для класифікації об'єктів на nb_classes
@@ -28,20 +25,51 @@ def get_model(nb_classes=100):
         layer.trainable = False
 
     # для finetuning ми використаємо звичайний SGD з малою швидкістю навчання та моментом
-    m.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+    m.compile(
+        optimizer=SGD(lr=0.0001, momentum=0.9),
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
     m.summary()
     return m
 
 
+# кількість класів, підставте ваше значення
+nb_classes = 42
+
 model = get_model(nb_classes=nb_classes)
-import random
-X = np.random.random((512,224,224,3))
-y = np.zeros((512,800))
 
-for i in range(0,512):
-    y[i,random.randint(0,799)] = 1
+# при необхідності завантажити ваги:
+# model.load_weights('weights_finetuned.h5')
 
-model.fit(X,y, batch_size=16, validation_split=0.05, nb_epoch=42,
-          callbacks=[ModelCheckpoint('weights_finetuned.h5', save_best_only=True, monitor='val_loss')])
+img_height = 224
+img_width = 224
+batch_size = 8
+
+# розділити датасет на тренувальний та тестовий
+# у пропорції 90/10
+train_dir = ''
+test_dir = ''
+
+# зробити генератор за рекомендаціями статті:
+# https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+
+train_gen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+train_generator = train_gen.flow_from_directory(
+    train_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='binary')
+
+model.fit_generator(
+    generator=train_generator,
+    # validation_data= напишіть генератор для тестових даних
+    steps_per_epoch=42,
+    nb_epoch=42,
+    callbacks=[ModelCheckpoint('weights_finetuned.h5', save_best_only=True, monitor='val_loss')])
+
 model.save_weights('weights_finetuned.h5')
-print('ololo')
