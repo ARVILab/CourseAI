77e34811133919a1dcf517a2934d5f8229e59b1f
exipilis <malenko.andrii@gmail.com>
2017-10-11 20:50:29 +0300

    cleanup

--- week2/SSD/train.py
+++ week2/SSD/train.py
@@ -18,24 +18,24 @@ bboxer = BBoxUtility(num_classes=81, priors=ssd300_priors)
 cocodata = COCO('/DATA/COCO/annotations/instances_train2017.json')
 cocoDir = '/DATA/COCO/train2017/'
 catsToIds = {}
+
 for i, catid in enumerate(cocodata.getCatIds()):
     catsToIds[catid] = i
 
 
-print('ololo')
 def generator(batch_size=4):
     imgList = cocodata.imgs.keys()
     imgcount =len(imgList)
     n=0
     while True:
-        X = np.zeros((batch_size, 300,300,3), dtype=np.float)
+        X = np.zeros((batch_size, 300, 300, 3), dtype=np.float)
         Y = []
         for i in range(0, batch_size):
             n = n%imgcount
-            imgData = cocodata.imgs[imgList[n]]
-            imgPath = cocoDir + imgData['file_name']
+            img_data = cocodata.imgs[imgList[n]]
+            imgPath = cocoDir + img_data['file_name']
             img = cv2.imread(imgPath)
-            X[i] = cv2.resize(img, (300,300), interpolation=cv2.INTER_CUBIC).astype(np.float) / 127.5 - 1.
+            X[i] = cv2.resize(img, (300, 300), interpolation=cv2.INTER_CUBIC).astype(np.float) / 127.5 - 1.
             anns = cocodata.imgToAnns[imgList[n]]
             bboxList = []
             for ann in anns:
@@ -58,11 +58,12 @@ def generator(batch_size=4):
                 # assign box format ([xmin,ymin,xmax,ymax] + [one_hot(80)])
                 classes = np.zeros(80)
                 classes[catsToIds[ann['category_id']]] = 1
-                bboxList.append(np.concatenate((bbox,classes)))
+                bboxList.append(np.concatenate((bbox, classes)))
             Y.append(bboxer.assign_boxes(np.array(bboxList)))
-            n+=1
+            n += 1
         Y = np.array(Y)
         yield X, Y
+
 
 g = generator(batch_size=2)
 
