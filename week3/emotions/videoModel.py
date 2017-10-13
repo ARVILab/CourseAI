d2f7220db71f56349f5ba42ac03e0617ce659594
exipilis <malenko.andrii@gmail.com>
2017-10-13 22:32:19 +0300

    cleanup

--- week3/emotions/videoModel.py
+++ week3/emotions/videoModel.py
@@ -2,7 +2,8 @@ from vggFace import VGGFace
 from keras.models import Model, Sequential
 from keras.layers import TimeDistributed, Conv2D, Flatten, LSTM, Dense, Input
 
-def getModel(frameCount = 42, nb_classes=7):
+
+def getModel(frameCount=42, nb_classes=7):
 
     vggFaceEncoder = VGGFace(include_top=False, input_shape=(224,224,3))
     for layer in vggFaceEncoder.layers:
@@ -10,11 +11,11 @@ def getModel(frameCount = 42, nb_classes=7):
 
     frameEncoder = Sequential()
     frameEncoder.add(vggFaceEncoder)
-    frameEncoder.add(Conv2D(256, kernel_size=(3,3), activation='relu', padding='valid'))
+    frameEncoder.add(Conv2D(256, kernel_size=(3, 3), activation='relu', padding='valid'))
     frameEncoder.add(Flatten())
     frameEncoder.add(Dense(512, activation='relu'))
 
-    inp = Input((frameCount,224,224,3))
+    inp = Input((frameCount, 224, 224, 3))
     seqFeatures = TimeDistributed(frameEncoder)(inp)
     lstmFeatures = LSTM(128)(seqFeatures)
     out = Dense(nb_classes, activation='softmax')(lstmFeatures)
