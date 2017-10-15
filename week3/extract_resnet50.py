a2243fd1c94bce07b958bccad3b1f309b9b1f55c
exipilis <malenko.andrii@gmail.com>
2017-10-15 15:41:31 +0300

    extract resnet50 features

---
+++ week3/extract_resnet50.py
@@ -0,0 +1,57 @@
+# extract resnet50 features using multiprocessing
+
+from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.layers import Flatten
+from keras.models import Model
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
+import numpy as np
+from scipy.misc import imread, imresize
+import os
+
+
+batch_size = 64
+
+# resnet50 + flatten
+
+resnet = ResNet50(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
+x = Flatten()(resnet.output)
+model = Model(inputs=resnet.input, outputs=x)
+model.compile('adam', 'mse')
+
+
+# from - to dirs
+
+in_path = '../datasets/coco/train2017/'
+out_path = 'data/resnet50_vectors/'
+
+# filter images
+
+fns = [fn for fn in os.listdir(in_path) if fn.endswith('.jpg')]
+n = len(fns)
+
+
+def f(s):
+    img = imread(in_path + s, mode='RGB')
+    img = imresize(img, (224, 224))
+    return img
+
+
+pool = ThreadPool(cpu_count())
+
+
+for i in range(0, n, batch_size):
+    k = min(n, i + batch_size)
+
+    batch = pool.map(f, fns[i: k])
+
+    x = np.array(batch, dtype=np.float)
+    x = preprocess_input(x)
+
+    y = model.predict(x)
+
+    for j in range(i, k):
+        fn = fns[j]
+        np.save(out_path + fn + '.npy', y[j-i])
+
+    print(i)
