2eee1262da524576c7bbe8e826938eadb2b86da6
exipilis <malenko.andrii@gmail.com>
2017-10-18 14:56:03 +0300

    check week1 task

---
+++ week1/check_task1.py
@@ -0,0 +1,19 @@
+true_labels = []
+with open('classifieds_labels.txt') as f:
+    for s in f:
+        s = s.strip().split(' ', 1)[1].split('_')[0]
+        true_labels.append(int(s))
+
+n = len(true_labels)
+test_labels = [-1] * n
+
+with open('test_udod.txt') as f:
+    for s in f:
+        path, class_no = s.strip().split(' ')
+        i = int(path.split('/')[-1].split('.')[0])
+        test_labels[i] = int(class_no)
+
+result = [true_labels[i] == test_labels[i] for i in range(n)]
+
+c = sum(result)
+print('accuracy: %.02f%%' % (100*c/n))
