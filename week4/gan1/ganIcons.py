8354f126e92e04116f1260b5cf9a7ad2bb77ef5e
exipilis <malenko.andrii@gmail.com>
2017-10-22 12:36:28 +0300

    correct paths

--- week4/gan1/ganIcons.py
+++ week4/gan1/ganIcons.py
@@ -1,5 +1,4 @@
 import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
 
 from keras.models import Model
 from keras.layers import Dense, Activation, Flatten, Reshape, Input
@@ -10,6 +9,7 @@ from keras.optimizers import RMSprop
 
 import matplotlib.pyplot as plt
 import cv2
+
 
 # discriminator
 def discriminator_model():
@@ -47,14 +47,14 @@ def discriminator_model():
 
     x_label = Dense(41, activation='softmax')(x_f)
 
-    return Model(inputs=_inp, outputs=[x,x_label])
+    return Model(inputs=_inp, outputs=[x, x_label])
 
 
 def generator_model():
     _inp = Input(shape=(100,))
     _inp_label = Input((41,))
 
-    x_label = Dense(8*8*128)(_inp_label)
+    x_label = Dense(8 * 8 * 128)(_inp_label)
     x_label = Activation('relu')(x_label)
     x_label = Reshape((8, 8, 128))(x_label)
     x_label = Dropout(0.4)(x_label)
@@ -65,7 +65,7 @@ def generator_model():
     x = Reshape((8, 8, 256))(x)
     x = Dropout(0.4)(x)
 
-    x = Concatenate(axis=-1)([x,x_label])
+    x = Concatenate(axis=-1)([x, x_label])
 
     x = UpSampling2D()(x)
     x = Conv2DTranspose(256, 5, padding='same')(x)
@@ -93,33 +93,40 @@ def generator_model():
     x = Conv2DTranspose(3, 3, padding='same')(x)
     x = Activation('tanh')(x)
 
-    return Model(inputs=[_inp,_inp_label], outputs=x)
+    return Model(inputs=[_inp, _inp_label], outputs=x)
 
 
 discriminator = discriminator_model()
-discriminator.compile(loss=['binary_crossentropy','mse'], optimizer=RMSprop(lr=0.0002, decay=6e-8), metrics=['accuracy'])
+discriminator.compile(
+    loss=['binary_crossentropy', 'mse'],
+    optimizer=RMSprop(lr=0.0002, decay=6e-8),
+    metrics=['accuracy'])
 
 generator = generator_model()
 
 inp_z = Input((100,))
 inp_l = Input((41,))
-g = generator([inp_z,inp_l])
+g = generator([inp_z, inp_l])
 d = discriminator(g)
 adversarial = Model(inputs=[inp_z, inp_l], outputs=d)
-adversarial.compile(loss=['binary_crossentropy', 'mse'], optimizer=RMSprop(lr=0.0001, decay=3e-8), metrics=['accuracy'])
+adversarial.compile(
+    loss=['binary_crossentropy', 'mse'],
+    optimizer=RMSprop(lr=0.0001, decay=3e-8),
+    metrics=['accuracy'])
 
 
-inconNames = np.load('/DATA/CourseAI/datasets/icons/X.npy')
+inconNames = np.load('../../datasets/icons/X.npy')
 X = []
-for i in range(0,len(inconNames)):
-    img = cv2.imread('/DATA/CourseAI/datasets/icons/smallIcons/'+inconNames[i]+'.png')
-    img = img.astype(np.float)/127.5 -1
-    X.append(img[:,:,:3])
+for icon_name in inconNames:
+    img = cv2.imread('../../datasets/icons/smallIcons/' + icon_name + '.png')
+    img = img.astype(np.float)/127.5 - 1
+    X.append(img[:, :, :3])
 
 x_train = np.asarray(X)
-x_labels = np.load('/DATA/CourseAI/datasets/icons/Y.npy')
+x_labels = np.load('../../datasets/icons/Y.npy')
 idxs = np.random.randint(0, x_train.shape[0], size=16)
-test_lbls = x_labels[idxs,:]
+test_lbls = x_labels[idxs, :]
+
 
 def train(train_steps=200000, batch_size=256, save_interval=0):
     noise_input = None
@@ -130,7 +137,7 @@ def train(train_steps=200000, batch_size=256, save_interval=0):
     for i in range(train_steps):
         idxs = np.random.randint(0, x_train.shape[0], size=batch_size)
         images_train = x_train[idxs, :, :, :]
-        lbls_train = x_labels[idxs,:]
+        lbls_train = x_labels[idxs, :]
         noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
 
         images_fake = generator.predict([noise, lbls_train])
@@ -153,6 +160,7 @@ def train(train_steps=200000, batch_size=256, save_interval=0):
             if (i + 1) % save_interval == 0:
                 plot_images(save2file=True, samples=noise_input.shape[0], noise=noise_input, step=(i + 1))
                 adversarial.save_weights('g_d_icons.h5')
+
 
 def plot_images(save2file=False, fake=True, samples=16, noise=None, step=0):
     filename = 'icons.png'
