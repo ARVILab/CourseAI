5e60d4b043a390b1cc81668c059d19a44d73e7dd
exipilis <malenko.andrii@gmail.com>
2017-10-28 16:24:47 +0300

    cleanup

--- week5/docEncoder/trainW2v.py
+++ week5/docEncoder/trainW2v.py
@@ -3,7 +3,7 @@ import gensim.models
 print('loading sentences...')
 sentences = gensim.models.word2vec.LineSentence("../../datasets/nlp/all.txt")
 
-model = gensim.models.Word2Vec(min_count=15,size=200)
+model = gensim.models.Word2Vec(min_count=15, size=200)
 model.build_vocab(sentences)
 
 model.train(sentences)
