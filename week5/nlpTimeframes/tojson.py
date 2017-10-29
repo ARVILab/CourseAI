f4caabb8db0e26dac263c25aa05f67c5f5e291a9
exipilis <malenko.andrii@gmail.com>
2017-10-29 16:52:08 +0200

    cleanup

--- week5/nlpTimeframes/tojson.py
+++ week5/nlpTimeframes/tojson.py
@@ -1,25 +1,28 @@
 __author__ = 'oles'
 import json
-import numpy as np
+
 years = []
 startYear = 1950
 lastYear = 2005
 name = 'subtitlesEn'
+
 for i in range(startYear, lastYear+1):
-    with open('UI/static/3d/'+str(i)+'.obj', 'r') as yearfile:
+    with open('UI/static/3d/' + str(i) + '.obj', 'r') as yearfile:
         years.append([])
         for s in yearfile:
             if s[0] == 'v':
                 v = s.split()[-3:]
                 for k in v:
-                    years[i-startYear].append(np.round(float(k), 4))
+                    years[i-startYear].append(round(float(k), 4))
 
-jsonObject = { 'name':name,
-               'startYear':startYear,
-                'lastYear':lastYear,
-               'years':[]
+jsonObject = {
+    'name': name,
+    'startYear': startYear,
+    'lastYear': lastYear,
+    'years': []
 }
-for i in range(0,len(years)):
-    jsonObject["years"].append(years[i])
 
-json.dump(jsonObject, open('UI/models/'+name +'.json','w'))
+for y in years:
+    jsonObject["years"].append(y)
+
+json.dump(jsonObject, open('UI/models/' + name + '.json', 'w'))
