09d31c335447dbb7d4bc4231412ad11a11813e3d
exipilis <malenko.andrii@gmail.com>
2017-10-29 16:03:50 +0200

    cleanup

--- week5/nlpTimeframes/timeModels.py
+++ week5/nlpTimeframes/timeModels.py
@@ -14,7 +14,7 @@ for year in range(1950, 2015):
     print('loading sentences...')
     sentences = gensim.models.word2vec.LineSentence('../../datasets/nlp/subs/concat/%s.txt' % year)
     print('training...')
-    tempModel  = gensim.models.Word2Vec(min_count=15,size=1)
+    tempModel = gensim.models.Word2Vec(min_count=15,size=1)
     tempModel.build_vocab(sentences)
     model.train(sentences, total_examples=tempModel.corpus_count, epochs=tempModel.iter)
     model.save('UI/models/' + str(year))
