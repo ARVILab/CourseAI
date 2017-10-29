1c7a3df9a99cdc77a089a4d04cde23726c80cda2
php3ik <oles.petriv@gmail.com>
2017-10-29 16:06:03 +0200

    dynamic timeframe file

--- week5/nlpTimeframes/concat.py
+++ week5/nlpTimeframes/concat.py
@@ -3,12 +3,12 @@ import os
 with open('../../datasets/nlp/subs/concat/all.txt', 'w') as allfile:
     for i in range(1948, 2002):
         s = ''
-        with open('../../datasets/nlp/subs/concat/%s.txt' % (i+2), 'w') as of:
-            for j in range(i, i+5):
-                fn = '../../datasets/nlp/subs/en/%s.txt' % j
-                if os.path.isfile(fn):
-                    with open(fn,'r') as f:
-                        l = f.read()
-                        of.write(l)
-                        allfile.write(l)
-            print(i)
+        #with open('../../datasets/nlp/subs/concat/%s.txt' % (i+2), 'w') as of:
+        for j in range(i, i+5):
+            fn = '../../datasets/nlp/subs/en/%s.txt' % j
+            if os.path.isfile(fn):
+                with open(fn,'r') as f:
+                    l = f.read()
+                #   of.write(l)
+                    allfile.write(l)
+        print(i)
--- week5/nlpTimeframes/timeModels.py
+++ week5/nlpTimeframes/timeModels.py
@@ -1,6 +1,7 @@
 import numpy as np
 import gensim.models
 import logging
+import os
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 print('loading sentences...')
@@ -13,7 +14,15 @@ model.build_vocab(sentences)
 for year in range(1950,2005):
 	print('training model for year: ' + str(year))
 	print('loading sentences...')
-	sentences = gensim.models.word2vec.LineSentence('../../datasets/nlp/subs/concat/%s.txt' % year)
+	with open('../../datasets/nlp/subs/timeframeFile.txt','w') as timeframeFile:
+		for j in range(year-2, year + 3):
+			fn = '../../datasets/nlp/subs/en/%s.txt' % j
+			if os.path.isfile(fn):
+				with open(fn, 'r') as f:
+					timeframeFile.write(f.read())
+
+
+	sentences = gensim.models.word2vec.LineSentence('../../datasets/nlp/subs/timeframeFile.txt')
 	print('training...')
 
 	tempModel  = gensim.models.Word2Vec(min_count=15,size=1)
