3918c6df5f272d2f1ec5cf6e5c43b0218b653a89
php3ik <oles.petriv@gmail.com>
2017-10-29 15:22:18 +0200

    concat subs

---
+++ week5/nlpTimeframes/concat.py
@@ -0,0 +1,11 @@
+import os
+
+for i in range(1948, 2012):
+    s = ''
+    with open('../../datasets/nlp/subs/concat/%s.txt' % (i+2), 'w') as of:
+        for j in range(i, i+5):
+            fn = '../../datasets/nlp/subs/en/%s.txt' % j
+            if os.path.isfile(fn):
+                with open(fn,'r') as f:
+                    of.write(f.read())
+        print(i)
