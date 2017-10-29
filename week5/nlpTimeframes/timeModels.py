defda97426a0bd47c794a49e49786a5e3f463466
exipilis <malenko.andrii@gmail.com>
2017-10-29 16:04:18 +0200

    cleanup

--- week5/nlpTimeframes/timeModels.py
+++ week5/nlpTimeframes/timeModels.py
@@ -9,12 +9,12 @@ sentences = gensim.models.word2vec.LineSentence("../../datasets/nlp/subs/concat/
 model = gensim.models.Word2Vec(min_count=15, size=200)
 model.build_vocab(sentences)
 
-for year in range(1950, 2015):
+for year in range(1950, 2012):
     print('training model for year: ' + str(year))
     print('loading sentences...')
     sentences = gensim.models.word2vec.LineSentence('../../datasets/nlp/subs/concat/%s.txt' % year)
     print('training...')
-    tempModel = gensim.models.Word2Vec(min_count=15,size=1)
+    tempModel = gensim.models.Word2Vec(min_count=15, size=1)
     tempModel.build_vocab(sentences)
     model.train(sentences, total_examples=tempModel.corpus_count, epochs=tempModel.iter)
     model.save('UI/models/' + str(year))
