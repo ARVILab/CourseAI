dc3b199f0e6be634dc3dcbeab347a1c52b450ea1
exipilis <malenko.andrii@gmail.com>
2017-10-29 16:56:35 +0200

    cleanup

--- week5/nlpTimeframes/tsneEn.py
+++ week5/nlpTimeframes/tsneEn.py
@@ -6,15 +6,23 @@ import logging
 import json
 import os
 import numpy as np
+
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+top_words = 7500
+
 
 def save3d(vectors3d, name):
     print('saving 3d vectors for model :' + str(name))
 
-    f = open('UI/static/3d/'+str(name)+'.obj','w')
+    f = open('UI/static/3d/' + str(name) + '.obj', 'w')
     f.write('g language\n')
-    for i in range (0, 7500) :
-        f.write( 'v ' + str(float(vectors3d[i][0])) + ' ' + str(float(vectors3d[i][1])) + ' ' + str(float(vectors3d[i][2])) + '\n')
+
+    for i in range(top_words):
+        f.write('v ' +
+                str(float(vectors3d[i][0])) + ' ' +
+                str(float(vectors3d[i][1])) + ' ' +
+                str(float(vectors3d[i][2])) + '\n')
     f.close()
 
 
@@ -22,12 +30,12 @@ tsnemodel = TSNE(n_components=3, verbose=3, perplexity=42, random_state=0, init=
 
 model = gensim.models.Word2Vec().load('UI/models/1950')
 with open('UI/static/3d/words.js', 'w') as outfile:
-    out = 'words =' + json.dumps(model.index2word[:7500])
+    out = 'words =' + json.dumps(model.index2word[:top_words])
     outfile.write(out)
 
-for year in range(1950,2005):
-    if os.path.isfile('models/'+str(year)):
-        model = np.load('models/'+str(year)+'.wv.syn0.npy')[:7500]
+for year in range(1950, 2005):
+    if os.path.isfile('models/' + str(year)):
+        model = np.load('models/' + str(year) + '.wv.syn0.npy')[:top_words]
         X = StandardScaler().fit_transform(model)
         # if year > 1905:
         #     tsnemodel.n_iter = 75
