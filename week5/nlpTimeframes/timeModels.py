6b62f10e00f390f24324240434d3a63329f39c84
exipilis <malenko.andrii@gmail.com>
2017-10-29 15:58:20 +0200

    cleanup

--- week5/nlpTimeframes/timeModels.py
+++ week5/nlpTimeframes/timeModels.py
@@ -1,6 +1,6 @@
-import numpy as np
 import gensim.models
 import logging
+
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 print('training model for year: 2007')
@@ -14,10 +14,11 @@ sentences = gensim.models.word2vec.LineSentence("data/years/2007.txt")
 model.train(sentences)
 model.save('models/focus_2007')
 
-for year in range(2008,2017):
-	print('training model for year: ' + str(year))
-	print('loading sentences...')
-	sentences = gensim.models.word2vec.LineSentence("data/years/"+str(year)+".txt")
-	print('training...')
-	model.train(sentences)
-	model.save('models/focus_'+str(year))
+for year in range(1950, 2014):
+    print('training model for year: ' + str(year))
+    print('loading sentences...')
+    sentences = gensim.models.word2vec.LineSentence("data/years/"+str(year)+".txt")
+    print('training...')
+    model.train(sentences)
+    model.save('models/focus_' + str(year))
+
